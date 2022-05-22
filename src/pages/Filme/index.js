b3f5e83d32f106b50eb89b7ebb44f0344a16c44f
Gabriel Moreira <83798870+gabrielms00777@users.noreply.github.com>
2022-05-22 17:35:47 -0300

    Add files via upload

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import './filme-info.css'
+import { toast } from 'react-toastify'
+
+import api from '../../services/api'
+
+function Filme(){
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const [ filme, setFilme ] = useState({})
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: '12ae37a6d91f1b47d5357bc6def472ab',
+                    language: 'pt-BR',
+                }
+            })
+            .then((response)=>{
+                //console.log(response.data)
+                setFilme(response.data)
+                //console.log(response.data)
+                setLoading(false)
+            })
+            .catch(()=>{
+                //console.log('FILME NÃO ENCONTRADO')
+                navigate('/', { replace:true })
+                return
+            })
+            
+        }
+
+        loadFilme()
+
+        return()=>{
+            //console.log('COMPONENTE FOI DESMONTADO')
+        }
+
+    }, [navigate,id])
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carrengando detalhes...</h1>
+            </div>
+        )
+    }
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix")
+
+        let filmesSalvos = JSON.parse(minhaLista) || []
+
+        const hasFilme = filmesSalvos.some( (filmeSalvo)=> filmeSalvo.id === filme.id)
+
+        if(hasFilme){
+            toast.warn('Esse filme já esta na sua lista!')
+            return
+        }
+
+        filmesSalvos.push(filme)
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+        toast.success('FILME SALVO COM SUCESSO!')
+
+    }
+
+
+    return(
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button onClick={salvarFilme}>Salvar</button>
+                <button><a target='blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a></button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Filme;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+import { Link } from 'react-router-dom'
+import './home.css'
+
+//URL DA API: /movie/now_playing?api_key=12ae37a6d91f1b47d5357bc6def472ab&language=pt-BR
+
 function Home(){
+
+    const [ filmes, setFilmes ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get('movie/now_playing', {
+                params:{
+                    api_key: '12ae37a6d91f1b47d5357bc6def472ab',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            //console.log(response.data.results.slice(0, 10))
+            setFilmes(response.data.results.slice(0, 10))
+        }
+
+
+        loadFilmes()
+        setLoading(false)
+
+    }, [])
+
+
+    if(loading){
+        return(
+            <div className='loading'>
+                <h2>Carrengando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Pagina Home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+            {filmes.map((filme)=>{
+                return(
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                    </article>
+                )
+            })}
+            </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
